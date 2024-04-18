bb200d74d6fb2ad7f5c3d4771626ea0efedd433d
leomeden <leonardomeden@gmail.com>
2024-04-18 15:59:52 -0300

    Prueba httpClient exitosa

--- src/MoneyExchange.java
+++ src/MoneyExchange.java
@@ -1,5 +1,20 @@
+import com.google.gson.*;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
 public class MoneyExchange {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, InterruptedException {
         System.out.println("Comenzando la App");
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("https://v6.exchangerate-api.com/v6/b1d4e2b12c4a0efd19f853e1/pair/USD/ARS"))
+                .build();
+        HttpResponse<String> response = client
+                .send(request, HttpResponse.BodyHandlers.ofString());
+        System.out.println(response.body());
     }
 }
